383e36f33c90788dba8925b054e9f6105e705bdd
ISU Student <labs@irnok.net>
2018-03-19 13:16:58 +0800

    Implemented partially main body.

--- src/main/java/org/isu/oberon/Main.java
+++ src/main/java/org/isu/oberon/Main.java
@@ -1,7 +1,22 @@
 package org.isu.oberon;
 
+import org.antlr.v4.runtime.ANTLRInputStream;
+
+import java.io.FileInputStream;
+
 public class Main {
     public static void main(String[] argv ) {
-        System.out.println("Oberon Java compiler");
-    }
+        try {
+            ANTLRInputStream input = new ANTLRInputStream(
+                    new FileInputStream(args[0]));
+
+            GYOOLexer lexer = new GYOOLexer(input);
+            GYOOParser parser = new GYOOParser(new CommonTokenStream(lexer));
+            parser.addParseListener(new MyListener());
+
+            // Start parsing
+            parser.program();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }    }
 }
