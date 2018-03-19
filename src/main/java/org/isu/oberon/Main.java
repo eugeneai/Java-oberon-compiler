cbb2ebf79859c2a7a13c1e8a5b5f2c386d247f16
Evgeny Cherkashin <eugene@irnok.net>
2018-03-19 19:41:01 +0800

    The first successful run of wrong grammar.

--- src/main/java/org/isu/oberon/Main.java
+++ src/main/java/org/isu/oberon/Main.java
@@ -1,22 +1,31 @@
 package org.isu.oberon;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] argv ) {
+    public static void main(String[] args ) {
+        // Show where we are.
+        System.out.println("Working Directory = " +
+                System.getProperty("user.dir"));
         try {
+
             ANTLRInputStream input = new ANTLRInputStream(
                     new FileInputStream(args[0]));
 
-            GYOOLexer lexer = new GYOOLexer(input);
-            GYOOParser parser = new GYOOParser(new CommonTokenStream(lexer));
-            parser.addParseListener(new MyListener());
+            ExprLexer lexer = new ExprLexer(input);
+            ExprParser parser = new ExprParser(new CommonTokenStream(lexer));
+            // parser.addParseListener(new MyListener());
+
 
             // Start parsing
-            parser.program();
+            parser.expression();
+
         } catch (IOException e) {
             e.printStackTrace();
-        }    }
+        }
+    }
 }
